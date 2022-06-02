8c66f52bef4b9b44ba972abd25ae354f10a5583c
Matheus Pires <matheushenriquepires99@gmail.com>
2022-06-02 08:45:38 -0300

    feat: header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Header } from './components/header';
+
+import './styles/global.css';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/header/header.jsx
@@ -0,0 +1,11 @@
+import logo from '../../assets/logo.svg';
+
+import styles from './styles.module.css';
+
+export const Header = () => {
+  return (
+    <header className={styles.header}>
+      <img src={logo} alt="Logo Ignite" />
+    </header>
+  );
+};
