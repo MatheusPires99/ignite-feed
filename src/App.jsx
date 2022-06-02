f157ec150db42097b790dcd7374b952313c302e0
Matheus Pires <matheushenriquepires99@gmail.com>
2022-06-02 09:49:52 -0300

    feat: sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Header } from './components/header';
+import { Sidebar } from './components/sidebar';
 
 import './styles/global.css';
+
+import styles from './App.module.css';
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Hello World</h1>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>Hello World</main>
+      </div>
     </div>
   );
 }
---
+++ src/components/sidebar/sidebar.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import { PencilLine } from 'phosphor-react';
+
+import styles from './styles.module.css';
+
+export const Sidebar = () => {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8ZGV2ZWxvcGVyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <img src="https://github.com/MatheusPires99.png" alt="" />
+        <strong>Matheus Pires</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+};
