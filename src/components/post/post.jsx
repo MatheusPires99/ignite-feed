1cc4427562a781b682cb3d63cf8e8147d6f64804
Matheus Pires <matheushenriquepires99@gmail.com>
2022-06-08 09:45:00 -0300

    feat: Post props

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -1,39 +1,49 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBr from 'date-fns/locale/pt-BR';
+
 import { Avatar } from '../avatar';
 import { Comment } from '../comment';
 
 import styles from './styles.module.css';
 
-export const Post = () => {
+export const Post = ({ author, content, publishedAt }) => {
+  const dateFormatted = format(publishedAt, "dd 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBr,
+  });
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBr,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/MatheusPires99.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Matheus Pires</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="11 de maio às 08:13" dateTime="2022-05-11 08:13:30">
-          Publicado há 1h
+        <time title={dateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-        <p>
-          <a href="#">jane.design/doctorcare</a>
-        </p>
-        <p>
-          <a href="#">#novoprojeto</a>
-          <a href="#">#nlw</a>
-          <a href="#">#rocketseat</a>
-        </p>
+        {content.map(line => {
+          if (line.type === 'link') {
+            return (
+              <a key={line.content} href="#">
+                {line.content}
+              </a>
+            );
+          }
+
+          return <p key={line.content}>{line.content}</p>;
+        })}
       </div>
 
       <form className={styles.commentForm}>
